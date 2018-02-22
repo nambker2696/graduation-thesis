54461c59ac97be6ff1fe45aa7790416e3f50c996
nambker2696 <nguyenvannam20132696@gmail.com>
2018-02-23 01:47:43 +0700

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations'}
   scope "(:locale)", locale: /en|vi|ja/ do
-  devise_for :users
-  root to: "home#index"
+    devise_for :users, skip: :omniauth_callbacks
+    root to: "home#index"
   end
   root to: redirect("/#{I18n.default_locale}", status: 302),
   as: :redirected_root
