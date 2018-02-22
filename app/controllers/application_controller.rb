dc4161c43badecaf93322aa7912d31998f1a4b06
nambker2696 <nguyenvannam20132696@gmail.com>
2018-02-23 00:50:35 +0700

    layout user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
-  before_action :ensure_subdomain
+  # before_action :ensure_subdomain
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
