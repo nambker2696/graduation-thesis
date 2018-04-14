10528a848daeaefaf6f0aa55069f8fd8d8a8b9e4
nambker2696 <nguyenvannam20132696@gmail.com>
2018-04-14 11:30:05 +0700

    fix screenadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
    devise_for :users, only: :omniauth_callbacks, controllers:
   { omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'registrations' }
