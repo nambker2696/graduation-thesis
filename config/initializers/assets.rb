587630d7af22517b8b2a544ba9371daef72f340f
nambker2696 <nguyenvannam20132696@gmail.com>
2018-04-17 19:31:44 +0700

    store

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( admin.css )
-Rails.application.config.assets.precompile += %w( store.css )
 Rails.application.config.assets.precompile += %w( admin.js )
+Rails.application.config.assets.precompile += %w( store.css )
 Rails.application.config.assets.precompile += %w( store.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
