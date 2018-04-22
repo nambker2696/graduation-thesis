380d91849f834187b5c1b0710eb7b08873981fc1
nambker2696 <nguyenvannam20132696@gmail.com>
2018-04-22 20:20:07 +0700

    Seting location

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -1,75 +1,75 @@
 class Admin::LocationsController < Admin::BaseController
-  before_action :set_location, only: [:show, :edit, :update, :destroy]
+  before_action :set_admin_ocation, only: [:show, :edit, :update, :destroy]
 
-  # GET /locations
-  # GET /locations.json
+  # GET /admin_locations
+  # GET /admin_locations.json
   def index
     store = Store.find_by(user_id: current_user.id)
-    @locations = Location.joins(:store).where("stores.id" => 1)
+    @admin_locations = Location.joins(:store).where("stores.id" => 1)
     @openinghours =  OpeningHour.joins(:location).joins(:store).where("stores.id=?",store.id)
   end
-  # GET /locations/1
-  # GET /locations/1.json
+  # GET /admin_locations/1
+  # GET /admin_locations/1.json
   def show
   end
 
-  # GET /locations/new
+  # GET /admin_locations/new
   def new
-    @location = Location.new
+    @admin_location = Location.new
   end
 
-  # GET /locations/1/edit
+  # GET /admin_locations/1/edit
   def edit
   end
 
-  # POST /locations
-  # POST /locations.json
+  # POST /admin_locations
+  # POST /admin_locations.json
   def create
-    @location = Location.new(location_params)
+    @admin_location = Location.new(admin_location_params)
 
     respond_to do |format|
-      if @location.save
-        format.html { redirect_to @location, notice: 'Location was successfully created.' }
-        format.json { render :show, status: :created, location: @location }
+      if @admin_location.save
+        format.html { redirect_to admin_locations_url, notice: 'Location was successfully created.' }
+        format.json { render :show, status: :created, admin_location: @admin_location }
       else
         format.html { render :new }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
+        format.json { render json: @admin_location.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /locations/1
-  # PATCH/PUT /locations/1.json
+  # PATCH/PUT /admin_locations/1
+  # PATCH/PUT /admin_locations/1.json
   def update
     respond_to do |format|
-      if @location.update(location_params)
-        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
-        format.json { render :show, status: :ok, location: @location }
+      if @admin_location.update(admin_location_params)
+        format.html { redirect_to admin_locations_url, notice: 'Location was successfully updated.' }
+        format.json { render :show, status: :ok, admin_location: @admin_location }
       else
         format.html { render :edit }
-        format.json { render json: @location.errors, status: :unprocessable_entity }
+        format.json { render json: @admin_location.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /locations/1
-  # DELETE /locations/1.json
+  # DELETE /admin_locations/1
+  # DELETE /admin_locations/1.json
   def destroy
-    @location.destroy
+    @admin_location.destroy
     respond_to do |format|
-      format.html { redirect_to locations_url, notice: 'Location was successfully destroyed.' }
+      format.html { redirect_to admin_locations_url, notice: 'Location was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_location
-      @location = Location.find(params[:id])
+    def set_admin_location
+      @admin_location = Location.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def location_params
+    def admin_location_params
       params.require(:location).permit(:store_id, :name, :address, :phone, :status)
     end
 end
--- app/views/admin/locations/_location.json.jbuilder
+++ app/views/admin/locations/_location.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! location, :id, :created_at, :updated_at
-json.url location_url(location, format: :json)
+json.extract! admin_location, :id, :created_at, :updated_at
+json.url admin_location_url(admin_location, format: :json)
